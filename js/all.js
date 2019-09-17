127378b4716a766e531817ebc3395521503a3fd7
dkplass <35795191+dkplass@users.noreply.github.com>
2019-09-17 10:41:52 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -12,10 +12,11 @@
         let hourDeg = hour * 30 + minute * (30 / 60);
         let minuteDeg = minute * 6 + second * (6 / 60);
         let secondDeg = second * 6;
-
-        hourHand.style.transform = `rotate(${(hourDeg - 90)}deg)`;
+        
+        // 90度為圖片預設位置
+        hourHand.style.transform = `rotate(${(hourDeg - 90)}deg)`; // 90度為圖片預設位置
         minutehourHand.style.transform = `rotate(${minuteDeg}deg)`;
-        secondHand.style.transform = `rotate(${(secondDeg - 180)}deg)`;
+        secondHand.style.transform = `rotate(${(secondDeg - 180)}deg)`; // 180度為圖片預設位置
     }
 
     setInterval(clock, 1000);
